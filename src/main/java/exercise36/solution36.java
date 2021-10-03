968bb9db51fb903cd019141a153d1247c61facfd
JP Oliveira <joaopedro.costa.oliveira@gmail.com>
2021-10-03 18:55:16 -0400

    pseudocode for 36

---
+++ src/main/java/exercise36/solution36.java
@@ -0,0 +1,32 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 36 Solution
+ *  Copyright 2021 Joao Pedro Oliveira
+ */
+
+package exercise36;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class solution36 {
+
+    /*
+    make an arraylist to hold the numbers the user inputs
+    print "Enter a number: "
+    input = user input that gets put into the arraylist
+
+    determine that if the user inputs 'done' then stop the do/while loop for taking in user input
+    print out the list of numbers
+
+    create a function for the average, min, max, and standard deviation
+
+    after running the numbers in the list through each function
+    print out the average, min, max, and standard deviation
+
+
+
+     */
+
+    
+}
